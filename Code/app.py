2178520c2fed6a72f6700a9a4f95ed9fbb360516
Daniel Torres <daniale93@gmail.com>
2021-01-27 20:33:15 -0600

    Add 1st python file

---
+++ Code/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, render_template
+import pymongo
+from bson.json_util import dumps, loads 
+
+
+app = Flask(__name__)
+
+
+# The default port used by MongoDB is 27017
+# https://docs.mongodb.com/manual/reference/default-mongodb-port/
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+
+# Define the 'classDB' database in Mongo
+db = client.sunshine
+
+cities = db.sunshine_db.find()
+list_cities = list(cities)
+json_data = dumps(list_cities)
+
+# for city in cities:
+#     print(city)
+ 
+
+
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+
+@app.route("/map")
+def normal():
+    return "map"
+
+
+@app.route("/json")
+def jsonified():
+    return json_data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
